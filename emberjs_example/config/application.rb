fa9d8383df3c0f291ff51695368ea6bfdc83c199
Juan Antonio Chávez Valencia <cavjzz@gmail.com>
2013-03-15 15:19:28 -0600

    Added json API

--- emberjs_example/config/application.rb
+++ emberjs_example/config/application.rb
@@ -7,11 +7,16 @@ Bundler.require(*Rails.groups(assets: %w(development test)))
 
 module EmberjsExample
   class Application < Rails::Application
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
 
-    # Use haml as template generator by default
-    config.generators do |g|
-      g.template_engine :haml
-    end
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
 
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
   end
 end
